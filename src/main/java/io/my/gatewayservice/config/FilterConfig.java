6562556aa2a8b4ddd54929dae8143e5745f814c1
mysend12 <mysend12@naver.com>
2022-01-17 17:52:56 +0900

    게이트웨이 서버 생성

---
+++ src/main/java/io/my/gatewayservice/config/FilterConfig.java
@@ -0,0 +1,30 @@
+package io.my.gatewayservice.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(route -> route.path("/dev/api/user/**")
+                        .filters(filter ->filter
+                                .addRequestHeader("gateway-request", "dev-user-request")
+                                .addRequestHeader("gateway-response", "dev-user-response")
+                                .rewritePath("/dev/api/user/(?<segment>.*)", "/$\\{segment}")
+                        ).uri("lb://DEV-USER-SERVICE")
+                ).route(route -> route.path("/live/api/user/**")
+                        .filters(filter ->filter
+                                .addRequestHeader("gateway-request", "live-user-request")
+                                .addRequestHeader("gateway-response", "live-user-response")
+                                .rewritePath("/live/api/user/(?<segment>.*)", "/$\\{segment}")
+                        ).uri("lb://LIVE-USER-SERVICE")
+                ).build();
+    }
+
+
+}
