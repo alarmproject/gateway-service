c01d4deebc5649313c40736441693671963fded7
mysend12 <mysend12@naver.com>
2022-01-18 19:26:13 +0900

    유레카 설정 삭제

--- src/main/java/io/my/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/io/my/gatewayservice/GatewayServiceApplication.java
@@ -2,13 +2,8 @@ package io.my.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
